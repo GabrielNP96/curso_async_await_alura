ae97ab4ab5196e6458b8e53876cc0e033a51a5ef
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-02-16 03:09:14 +0000

    add

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload")
+
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, rejetc) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name})
+        }
+
+        leitor.onerror = () => {
+            rejetc(`Erro na leitura do arquivo ${arquivo.nome}`)
+        }
+
+        leitor.readAsDataURL(arquivo);
+    });
+}
